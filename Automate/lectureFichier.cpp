5d1338dc4a3e8a2f55061ef8cde534ac7d542634
Raphael Thibierge <raphael.thibierge@etu.u-bordeaux.fr>
2015-04-03 09:29:27 +0200

    debut de fonction

--- Automate/lectureFichier.cpp
+++ Automate/lectureFichier.cpp
@@ -5,5 +5,20 @@
 //  Created by Raphael Thibierge on 03/04/2015.
 //  Copyright (c) 2015 Raphael Thibierge. All rights reserved.
 //
+#include <string>
+#include <vector>
+#include <set>
+#include <list>
+#include <iostream>
+#include <fstream>
 
+using namespace std;
 
+void loadData(string nomDuFichier, int & nbEtatAutomate, list<int> & etatsInitiaux, list<int> & etatsTerminaux ){
+    fstream f ;
+    f.open(nomDuFichier, ios::in) ;
+    if (f.fail())
+        cout << "ouverture en lecture impossible " << endl ;
+    
+}
+    
