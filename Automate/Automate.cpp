6faeb8afdb656304ad800ab2cb715881adb5a265
Maxime Maheo <imaxame@gmail.com>
2015-04-03 09:53:11 +0200

    classe automate

---
+++ Automate/Automate.cpp
@@ -0,0 +1,9 @@
+//
+//  Automate.cpp
+//  Automate
+//
+//  Created by Maxime Maheo on 03/04/2015.
+//  Copyright (c) 2015 Maxime Maheo. All rights reserved.
+//
+
+#include "Automate.h"
---
+++ Automate/main.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  Automate
+//
+//  Created by Maxime Maheo on 03/04/2015.
+//  Copyright (c) 2015 Maxime Maheo. All rights reserved.
+//
+
+#include <stdio.h>
