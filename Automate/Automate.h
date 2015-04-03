a15b6de80b40d415cce11c1ff3906aa6333afa39
Maxime Maheo <imaxame@gmail.com>
2015-04-03 09:56:53 +0200

    ajout d'une classe qui marche

--- Automate/Automate.h
+++ Automate/Automate.h
@@ -11,4 +11,8 @@
 
 #include <stdio.h>
 
+class Automate{
+    
+};
+
 #endif /* defined(__Automate__Automate__) */
--- Automate/main.cpp
+++ Automate/main.cpp
@@ -7,3 +7,14 @@
 //
 
 #include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    cout << "Hello World" << endl;
+    
+    return EXIT_SUCCESS;
+}
