c0cea334f2a0f6f13130e9ae2e20151f2a828ee7
Maxime Maheo <imaxame@gmail.com>
2015-04-03 09:05:44 +0200

    ajout des fichiers de bases

---
+++ Automate/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Automate
+//
+//  Created by Maxime Maheo, Maxime Renaudon and Raphael Thibierge on 03/04/2015.
+//  Copyright (c) 2015 Maxime Maheo. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+int main(int argc, const char * argv[]) {
+
+    
+    
+    return EXIT_SUCCESS;
+}
